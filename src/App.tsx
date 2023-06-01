485f8d8cf03f5e86ae7893d36107c45a9bcdb3b8
Yevgeniya Kobrina <yevgeniya@exove.fi>
2023-06-01 22:51:46 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Button } from '@mui/material';
+import { Box } from "@mui/material";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Box className='App' sx={{bg: 'secundary'}}>
+          <header className="App-header">
+            <p>Hello!</p>
+            <Button variant="contained" color="primary">
+              Click me
+            </Button>
+          </header>
+        </Box>
   );
 }
 
