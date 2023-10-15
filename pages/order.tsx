4cc4752fae13be85ef53978487c04ff8a0b488fc
Yevgeniya Kobrina <yevgeniya@exove.fi>
2023-10-15 14:01:49 +0300

    W-8 Sending order to Drupal

--- pages/order.tsx
+++ pages/order.tsx
@@ -4,12 +4,13 @@ import { useContext, useEffect, useState} from "react";
 import CartContext from "../src/context/CartContext";
 import ButtonMain from "../src/components/buttons/ButtonMain";
 import ButtonSecondary from "../src/components/buttons/ButtonSecondary";
-import { FormData, ProductImage } from "../src/types";
+import { CartItem, FormData, ProductImage } from "../src/types";
 import CartSummary from "../src/components/cart/CartSummary";
 import CheckoutLayout from "../src/components/layout/checkout";
 import { useTranslation } from 'next-i18next';
 import { useRouter } from 'next/router';
 import fetchProducts from "./api/api_products";
+import { v4 } from 'uuid';
 
 export async function getServerSideProps({locale}: any) {
     const [ products, images ]  = await fetchProducts(locale);
@@ -20,6 +21,8 @@ export async function getServerSideProps({locale}: any) {
     }
 }
 
+const nodePostUrl = 'https://main-bvxea6i-33i32kvwbas3y.de-2.platformsh.site/node/';
+
 const OrderPage: React.FC<{images: ProductImage[]}> = ({ images }) => {
     const { t } = useTranslation();
     const router = useRouter();
@@ -29,18 +32,72 @@ const OrderPage: React.FC<{images: ProductImage[]}> = ({ images }) => {
     let subTotal = cart.reduce((accumulator, currentValue) => accumulator + currentValue.quantity * currentValue.price, 0.00) || 0.00;
     const deliveryCost = method != 'cash' ? DELIVERY_COST : 0.00
 
+    let orderData = '';
+    cart.map((cartItem: CartItem) => {
+        orderData = orderData + '<a href="/node/' + cartItem.id + '">' + cartItem.name + '</a>, ' + cartItem.quantity + 'kpl.\n'
+    });
+    orderData = orderData + 'Summa ' + subTotal + ' eur. Maksaa: ' + method;
+
     const [formData, setFormData] = useState<FormData>({} as FormData);
+    let contactData = formData.name + ' ' + formData.surname + ', ' + formData.phone;
 
     const handlePayClick = () : void => {
+        sendOrder();
         emptyCart();
-        localStorage.setItem('contactFormData', '');
         handleNext();
+        localStorage.setItem('contactFormData', '');
         router.push('/confirmation');
     }
 
     const handleBackClick = () : void => {
         handleBack();
         router.push('/delivery');
+    }
+
+    const sendOrder = async() => {
+        await PostData(orderData);
+    }
+
+    function PostData(orderData: string) {
+        let requestURL = nodePostUrl + '?_format=json';
+        return new Promise((resolve, reject) => {
+            fetch(requestURL, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                    'Authorization': `Basic ${btoa(`admin:admin`)}`,
+                },
+                body: JSON.stringify({
+                    'type': 'order',
+                    'field_order_id': {
+                        'value': v4()
+                    },
+                    'field_date': {
+                        'value': new Date().toISOString().split('.')[0] + '+5:00'
+                    },
+                    'title': {
+                        'value' : 'Order from ' + formData['name'] + ' on ' + new Date().toLocaleString()
+                    },
+                    'field_contact_information': {
+                        'value' : contactData,
+                    },
+                    'field_details': [{
+                        'value' : orderData,
+                        'format': 'full_html',
+                    }],
+                })
+            })
+            .then((response) => response.json())
+            .then((res) => {
+                resolve(res);
+            })
+            .catch((error) => {
+                console.log('error while creating the order', error)
+                reject(error);
+            });
+        });
     }
 
     useEffect(() => {
